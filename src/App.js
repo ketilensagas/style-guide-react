c9dbe312f1c7aa86c446a2970e861d60ab442398
Ketilen Sagas <ketilensagas@hotmail.com>
2023-10-27 20:34:04 -0300

    design dashboard

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import *  as React from 'react';
+import './App.css';
+import Dashboard from './Componentes/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard title="Meu Painel Inicial"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('teste título', () => {
+  render(<App />);
+  const testCase= document.querySelector('.App h1');
+  expect(testCase).toHaveTextContent(/meu painel inicial/i);
+});
